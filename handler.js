f11fed70c35b8caef315febd39e5412cee72dae4
Sam Elsamman <sam.elsamman@gmail.com>
2020-09-28 12:16:17 -0400

    API for covidtesting changed

--- handler.js
+++ handler.js
@@ -12,7 +12,7 @@ const csv = {
     globDeaths: [`${jsuPath}time_series_covid19_deaths_global.csv`, "deaths.csv"],
     usCases: [`${jsuPath}time_series_covid19_confirmed_US.csv`, "cases_US.csv"],
     usDeaths: [`${jsuPath}time_series_covid19_deaths_US.csv`, "deaths_US.csv"],
-    usTests: [`https://covidtracking.com/api/v1/states/daily.csv`, "tests_US.csv"],
+    usTests: [`https://api.covidtracking.com/v1/states/daily.csv`, "tests_US.csv"],
     globTests: [`https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/testing/covid-testing-all-observations.csv`, "tests.csv"],}
 
 module.exports.processCSV = async (event,context,callback) => {
